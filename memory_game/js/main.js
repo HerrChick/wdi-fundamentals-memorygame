305330d024adc206cdfd336729b349b205aa9234
Elliott M Chick <elliott@Elliotts-MacBook.local>
2019-06-16 19:48:24 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage:"images/queen-of-hearts.png",
+  },
+  {
+    rank:"queen",
+    suit:"diamonds",
+    cardImage:"images/queen-of-diamonds.png",
+  },
+  {
+    rank:"king",
+    suit:"hearts",
+    cardImage:"images/king-of-hearts.png",
+  },
+  {
+    rank:"king",
+    suit:"diamonds",
+    cardImage:"images/king-of-diamonds.png",
+  },
+];
 var cardsInPlay = [];
 
 function checkForMatch (){
@@ -14,9 +35,11 @@ function checkForMatch (){
 
 function flipCard (cardId) {
 
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("Suit is " + cards[cardId].suit);
+  console.log("Image Location is " + cards[cardId].cardImage);
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
 
   checkForMatch ();
 
